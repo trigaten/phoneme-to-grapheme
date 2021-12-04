e1102a8df7936a85da74f6119a8cf69f82f06314
Ryan Brown <ryan.allen.empires@gmail.com>
2021-12-04 18:51:52 -0500

    finished getting phonetics

--- main.py
+++ main.py
@@ -13,7 +13,7 @@ filename = "words_alpha.txt"
 
 fileDir = os.path.dirname(os.path.realpath('words_alpha.txt'))
 
-size_original = 100
+size_original = 1000 #Change this value to change the dataset size
 
 dictionary_file = open("words_alpha.txt").read()
 dictionary = dictionary_file.split("\n")
@@ -25,22 +25,29 @@ all_phonetics_tuples = [[]]
 def get_phonetics(size):
 
     rand_nums = np.random.randint(0, high=dict_len, size=size)
+
+    #Get the numbers from the data
     for num in rand_nums:
         word = dictionary[num]
         modified_url = url + word
         curr_page = rq.get(modified_url, headers=headers)
 
+        #Check that the page is valid before continuing
         if curr_page.status_code != 404:
-            print("added")
-            htmls.append(curr_page.content)
-
+            htmls.append(curr_page.content) #store HTML files just in case
+            #
             soup = bs4.BeautifulSoup(curr_page.content, "html.parser")
             match = re.search("<span class=\"pr\"> *\S*<", str(soup))
+
+            #Handle errors
             try:
                 phonetics = re.search("> *(\D)+<", match.group(0)).group(0)
                 phonetics = phonetics[1:len(phonetics) - 1].replace("-", "").replace("</span>", "")
+
                 if not "+" in phonetics:
                     all_phonetics_tuples.append([phonetics, word])
+                    print("added")
+
             except:
                 print("Something is wrong here")
 
@@ -54,12 +61,14 @@ def get_phonetics(size):
                     print("Num changed")
                     new_num = np.random.randint(0, high=dict_len)
 
+                #changes the base url to include the new word to get that word from the website
                 modified_url = url + dictionary[new_num]
                 curr_page = rq.get(modified_url, headers=headers)
+
                 #Check that the page is valid
                 if curr_page.status_code == 404:
                     print("Error 404")
-                    #If not valid, try another number combination
+                    #If the page was not valid, try another number combination
                     return get_dictionary_html()
 
                 else:
@@ -68,20 +77,28 @@ def get_phonetics(size):
                     soup = bs4.BeautifulSoup(web_result, "html.parser")
                     match = re.search("<span class=\"pr\"> *\S+", str(soup))
 
+                    #Not all of the pages were in the same format, so we remove potential errors
                     try:
+                        #Use regex to retrieve the word from the html file
                         phonetics = re.search("> *(\D)*<", match.group(0)).group(0)
                         phonetics = phonetics[1:len(phonetics) - 1].replace("-", "").replace("</span>", "")
+
+                        #Some words included a + in them, so we removed those.
                         if not "+" or "\"" in phonetics:
                             all_phonetics_tuples.append([phonetics, word])
+                            print("Added def")
 
                     except:
+                        #Show that an error has occured
                         print("something went wrong" )
-                    print("Added def")
                     return curr_page.content
 
             web_result = get_dictionary_html()
 
+#Main sequence to get all of the phonetics
 get_phonetics(size_original)
+
+#Keep getting more phonetics until the size is reached
 while len(all_phonetics_tuples) < size_original:
     size = size_original - len(all_phonetics_tuples)
     get_phonetics(size)
