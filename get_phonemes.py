cf3ae4ae7e6c41e090c186dd2cb5a84ce1420c97
Ryan Brown <ryan.allen.empires@gmail.com>
2021-12-05 04:59:14 -0500

    Made phonetics gathering code more efficient

--- get_phonemes.py
+++ get_phonemes.py
@@ -4,101 +4,73 @@ import requests as rq
 import re
 import numpy as np
 import bs4
-import sys
 import codecs
-import csv
-sys.stdout = codecs.getwriter("iso-8859-1")(sys.stdout, 'xmlcharrefreplace')
 
-headers = {'User-Agent': "Mozilla/5.0 (Windows NT 6.1; Win64; x64; rv:47.0) Gecko/20100101 Firefox/47.0"}
-
+#Basic Background Info
+headers = {'User-Agent': "Mozilla/5.0 (Macintosh; Intel Mac OS X x.y; rv:42.0) Gecko/20100101 Firefox/42.0"}
 url = "https://www.merriam-webster.com/dictionary/" #URL to get words and phonetics from
 filename = "words_alpha.txt"
-
 fileDir = os.path.dirname(os.path.realpath('words_alpha.txt'))
 
 size_original = 1000 #Change this value to change the dataset size
 
+
 dictionary_file = open("words_alpha.txt").read()
 dictionary = dictionary_file.split("\n")
 dict_len = len(dictionary)
-htmls = []
-all_phonetics_tuples = [[]]
+all_phonetics_tuples = []
 
 #This is the main function to get the html files of size "size"
 def get_phonetics(size):
 
-    rand_nums = np.random.randint(0, high=dict_len, size=size)
+    rand_nums = [] #used to prevent the same word from being included in the set more than once
+    while(len(rand_nums) < size):
+        def get_dictionary_html():
+            new_num = np.random.randint(0, high=dict_len)
 
-    #Get the numbers from the data
-    for num in rand_nums:
-        word = dictionary[num]
-        modified_url = url + word
-        curr_page = rq.get(modified_url, headers=headers)
-
-        #Check that the page is valid before continuing
-        if curr_page.status_code != 404:
-            htmls.append(curr_page.content) #store HTML files just in case
-            #
-            soup = bs4.BeautifulSoup(curr_page.content, "html.parser")
-            match = re.search("<span class=\"pr\"> *\S*<", str(soup))
-
-            #Handle errors
-            try:
-                phonetics = re.search("> *(\D)+<", match.group(0)).group(0)
-                phonetics = phonetics[1:len(phonetics) - 1].replace("-", "").replace("</span>", "")
-
-                if not "+" or "\"" in phonetics:
-                    all_phonetics_tuples.append([phonetics, word])
-                    print("added")
-
-            except:
-                print("Something is wrong here")
-
-        else:
-            #Returns the HTML
-            def get_dictionary_html():
+            #Find a new number that is not already in the list of random numbers
+            while new_num in rand_nums:
+                print("Num changed")
                 new_num = np.random.randint(0, high=dict_len)
 
-                #Find a number that is not in the set
-                while new_num in rand_nums:
-                    print("Num changed")
-                    new_num = np.random.randint(0, high=dict_len)
+            #Add the new, unique number to the rand_nums list
+            rand_nums.append(new_num)
+            #changes the base url to include the new word to get that word from the website
+            modified_url = url + dictionary[new_num]
+            curr_page = rq.get(modified_url, headers=headers)
 
-                #changes the base url to include the new word to get that word from the website
-                modified_url = url + dictionary[new_num]
-                curr_page = rq.get(modified_url, headers=headers)
+            #Check that the page is valid
+            if curr_page.status_code == 404:
+                print("Error 404")
+                #If the page was not valid, try another number combination
+                return get_dictionary_html()
 
-                #Check that the page is valid
-                if curr_page.status_code == 404:
-                    print("Error 404")
-                    #If the page was not valid, try another number combination
-                    return get_dictionary_html()
+            else:
+                #Return the new word and page
+                web_result = curr_page.content
+                soup = bs4.BeautifulSoup(web_result, "html.parser")
+                match = re.search("<span class=\"pr\"> *\S+", str(soup))
 
-                else:
-                    #Return the new word and page
-                    web_result = curr_page.content
-                    soup = bs4.BeautifulSoup(web_result, "html.parser")
-                    match = re.search("<span class=\"pr\"> *\S+", str(soup))
+                #Not all of the pages were in the same format, so we remove potential errors
+                try:
+                    #Use regex to retrieve the word from the html file
+                    phonetics = re.search(r"> *(\D)*<", match.group(0)).group(0)
+                    phonetics = phonetics[1:len(phonetics) - 1]
+                    phonetics = re.sub(r"( |\'|\[|\]|ˈ|\+|\"|\(|\)|ˌ|</span>|-)*", "", phonetics)
 
-                    #Not all of the pages were in the same format, so we remove potential errors
-                    try:
-                        #Use regex to retrieve the word from the html file
-                        phonetics = re.search("> *(\D)*<", match.group(0)).group(0)
-                        phonetics = phonetics[1:len(phonetics) - 1].replace("-", "").replace("</span>", "")
+                    #Some words included a + in them, so we removed those.
+                    if not "+" or not "\"" in phonetics:
+                        all_phonetics_tuples.append([str(phonetics), str(word)])
+                        print("Added def")
 
-                        #Some words included a + in them, so we removed those.
-                        if not "+" or "\"" in phonetics:
-                            all_phonetics_tuples.append([phonetics, word])
-                            print("Added def")
+                except:
+                    #Show that an error has occured
+                    print("something went wrong" )
+                return curr_page.content
 
-                    except:
-                        #Show that an error has occured
-                        print("something went wrong" )
-                    return curr_page.content
+        web_result = get_dictionary_html()
 
-            web_result = get_dictionary_html()
-
-#Main sequence to get all of the phonetics
+#Main function to get all of the phonetics
 get_phonetics(size_original)
 
 #Keep getting more phonetics until the size is reached
@@ -106,19 +78,13 @@ while len(all_phonetics_tuples) < size_original:
     size = size_original - len(all_phonetics_tuples)
     get_phonetics(size)
 
-csv = str(all_phonetics_tuples)
-
 #Manually make CSV because none of the default libraries work for phonetics
-csv_str = []
-
-for i in range(1, 999):
-    j = 2*i
-    csv_str.append(str(csv[j + 1])+ "," +str(csv[j+2]))
+csv = all_phonetics_tuples
 
 #Export csv_str as a utf-8-sig formated file separated by ","
 with codecs.open("phonemes-words.csv", "w", "utf-8-sig") as text_file:
-    for line in csv_str:
-        line = re.sub(r"( |\'|\[|\]|ˈ|\+|\"|\(|\)|ˌ)*", "", line)
+    for line in csv:
+        line = str(line)
         text_file.write(line+"\n")
 
 text_file.close()
