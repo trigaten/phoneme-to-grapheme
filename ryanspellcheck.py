90da4f057ce0eb0d4e79a4c29c8f82ae18121e38
Ryan Brown <ryan.allen.empires@gmail.com>
2021-12-20 16:59:33 -0500

    Spellcheck has been updated for integration in the final project

--- ryanspellcheck.py
+++ ryanspellcheck.py
@@ -20,24 +20,19 @@ rand_words = ["test", "tes", "roar", "ror", "hower", "ower", "hour", "gold", "go
 correct_list = ["test", "test", "roar", "roar", "hour", "hour", "hour", "gold", "gold"]
 #pass this a list of word string outputs to spellcheck and correct all of them
 
-def spellcheck_all(str_list, correct_list):
-    new_dict = [None] * len(str_list)
-    total_equal = 0
+def spellcheck_all(str_list):
+    new_list = [None] * len(str_list)
     str_list = list(str_list)
-    print(str_list)
 
     for i in range(len(str_list)):
-        if not str_list[i] in a_dict:
-            new_dict[i] = (spellcheck("".join(str_list[i])))
-            print("new_word: " + new_dict[i])
-        else:
-            new_dict[i] = str_list[i]
+        new_list[i] = spellcheck_1(str_list[i])
 
-        if new_dict[i] == str_list[i]:
-            total_equal += 1
+    print("original list: " + str(new_list))
+    return new_list
 
-    print("original list: " + str(new_dict))
-    difference = set(new_dict) - set(correct_list)
-    return (difference, total_equal/len(str_list))
-
-print(str(spellcheck_all(rand_words, correct_list)))
+def spellcheck_1(str):
+    spellchecked = str
+    if not str in a_dict:
+        spellchecked = (spellcheck("".join(str)))
+        print("new_word: " + spellchecked)
+    return spellchecked
