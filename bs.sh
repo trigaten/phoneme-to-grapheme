2306c33e418432c8e61c2481b7770c01cf91d5d6
trigaten <sanderman5@hotmail.com>
2021-12-20 08:53:17 -0500

    train log

--- bs.sh
+++ bs.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
+#SBATCH --time=20:00:00
 #SBATCH --partition=gpu
-#SBATCH --qos=gpu-short
+#SBATCH --qos=gpu-medium
 #SBATCH --gres=gpu:rtx2080ti:1
 echo $(hostname)
-python python.py
+python script.py
